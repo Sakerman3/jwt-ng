7a0610ba0642af680fd3b83349ec9c9b4759c7d2
Sven Akerman III <sakerman3@outlookinsight.com>
2025-08-19 16:04:37 -0700

    Updated env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
     tenantId: 'b39ef6b0-f4c4-4266-b067-f0ae2067d705',
     clientId: 'a0ad0e5a-783c-4479-aef8-877d5da4bef7',
     authority: 'https://login.microsoftonline.com/b39ef6b0-f4c4-4266-b067-f0ae2067d705',
-    redirectUri: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
+    redirectUri: 'https://zealous-pebble-0add19e0f-production.eastus2.1.azurestaticapps.net/',
+    postLogoutRedirectUri: 'https://zealous-pebble-0add19e0f-production.eastus2.1.azurestaticapps.net',
     apiScope: 'api://9d05af9d-cfdd-410a-8f84-c3842cd075c9/access_as_user'
   }
 };

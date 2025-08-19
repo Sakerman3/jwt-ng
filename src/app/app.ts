c6e990b916e9886e53cfe86956982dc44f6966cf
Sven Akerman III <sakerman3@outlookinsight.com>
2025-08-18 17:01:58 -0700

    MSAL fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MsalModule } from '@azure/msal-angular';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MsalModule],
-  template: `
-    <router-outlet></router-outlet>
-    <msal-redirect></msal-redirect>
-  `,
-  styleUrls: ['./app.scss'] // correct property name (plural)
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
+  styleUrls: ['./app.scss']
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('jwt-ng');
+
+  constructor(private msal: MsalService) {}
+
+  ngOnInit(): void {
+    // MSAL v3: after APP_INITIALIZER has run, process the redirect response once
+    this.msal.instance.handleRedirectPromise().catch(err => {
+      console.error('MSAL redirect handling error', err);
+    });
+  }
 }

88bd8a7f31bdf0c81b9b97eef845e4af4c634959
Sven Akerman III <sakerman3@outlookinsight.com>
2025-08-19 15:30:21 -0700

    Update environment redirect and logout

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,8 +10,8 @@ export const environment = {
     clientId: 'a0ad0e5a-783c-4479-aef8-877d5da4bef7',
     authority: `https://login.microsoftonline.com/b39ef6b0-f4c4-4266-b067-f0ae2067d705`,
     // Static Web Apps URL
-    redirectUri: 'https://zealous-pebble-0add19e0f.1.azurestaticapps.net',
-    postLogoutRedirectUri: 'https://zealous-pebble-0add19e0f-production.eastus2.1.azurestaticapps.net',
+    redirectUri: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
     apiScope: 'api://9d05af9d-cfdd-410a-8f84-c3842cd075c9/access_as_user'
   }
 };
